2e9ff82ebf293d93d4e59ac425bb5dcfa89464de
Borja Lopez <borja.lopez83@gmail.com>
2016-02-25 14:06:38 +0100

    Add handler and config empty classes

---
+++ src/main/java/io/pst/lambda/upload/s3/object/from/apigateway/LambdaConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2016 Pentasoft Sistemas SL.
+ */
+package io.pst.lambda.upload.s3.object.from.apigateway;
+
+/**
+ * Synchronization parameterization.
+ * 
+ * @author Borja Lopez Altarriba
+ *
+ */
+public class LambdaConfig {
+
+}
