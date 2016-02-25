2a2fb33fd79f786b9791868770c0c26861f7a387
Borja Lopez <borja.lopez83@gmail.com>
2016-02-25 10:03:46 +0100

    Archetype project.

---
+++ src/main/java/io/pst/lambda/upload/s3/object/from/apigateway/App.java
@@ -0,0 +1,13 @@
+package io.pst.lambda.upload.s3.object.from.apigateway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
